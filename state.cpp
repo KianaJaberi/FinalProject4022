c01fc0f49421dfe6c963a0fd5855890efe4b3919
KianaJaberi <152029042+KianaJaberi@users.noreply.github.com>
2024-06-13 19:06:40 +0330

    Add files via upload
    states with name

--- state.cpp
+++ state.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 class State {
+
     public:
         bool vicinityStatus ( int state1 , int state2 ){
             if ( states [state1][state2] == 1 ){
@@ -10,11 +11,32 @@ class State {
                 return false ;
             }
         }
+
         void setProprietor ( int player , int state ){
             proprietor [state] = player ;
         }
+        
         int getProprietor ( int state ){
             return proprietor [state] ;
+        }
+
+        std :: string getStateName ( int s ){
+            switch (s){
+                case 0 : return "BELLA" ;
+                case 1 : return "CALINE" ;
+                case 2 : return "ENNA" ;
+                case 3 : return "ATELA" ;
+                case 4 : return "PLADACI" ;
+                case 5 : return "BORGE" ;
+                case 6 : return "DIMASE" ;
+                case 7 : return "MORINA" ;
+                case 8 : return "OLIVADI" ;
+                case 9 : return "ROLLO" ;
+                case 10 : return "TALMONE" ;
+                case 11 : return "ARMENTO" ;
+                case 12 : return "LIA" ;
+                case 13 : return "ELINIA" ;
+            }
         }
 
     private:
