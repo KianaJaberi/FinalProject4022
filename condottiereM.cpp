166c51c06756d0b1cc14c64c53ab4a3a9a23418c
KianaJaberi <152029042+KianaJaberi@users.noreply.github.com>
2024-06-15 10:12:47 +0330

    Add files via upload
    new

--- condottiereM.cpp
+++ condottiereM.cpp
@@ -6,6 +6,7 @@
 class Condottiere {
 
     public :
+
         void setNumberOfPlayers (){
             int num ;
 
@@ -26,18 +27,22 @@ class Condottiere {
         }
 
         void setInformations (){
-            std :: string name ;
-            std :: string color1 ;
+
             int age ;
+            int smallestAge = 100 ;
+            std :: string input ;
             Player newPlayer ;
+            std :: vector < std :: string > colorsList = { "yellow" , "orange" , "red" ,
+                                                           "green"  , "blue"   , "purple" } ;
             HANDLE color ;
 	        color = GetStdHandle ( STD_OUTPUT_HANDLE ) ;
 
             for ( int i = 1 ; i <= numberOfPlayers ; i ++ ){
 
                 std :: cout << "Enter the name of player (" << i << ") : " ;
-                std :: cin >> name ;
-                newPlayer.setName (name) ;
+                std :: cin >> input ;
+
+                newPlayer.setName ( input ) ;
 
                 while ( true ){
                 
@@ -50,45 +55,105 @@ class Condottiere {
                     }
                     else {
                         newPlayer.setAge (age) ;
+
+                        if ( age < smallestAge ){
+                            smallestAge = age ;
+                            youngestPerson = i ;
+                        }
                         break ;
                     }
                 }
 
                 while ( true ){
-                    SetConsoleTextAttribute ( color , 14 ) ;
-                    std :: cout << "yellow , " ;
-                    SetConsoleTextAttribute ( color , 6 ) ;
-                    std :: cout << "orange , " ;
-                    SetConsoleTextAttribute ( color , 12 ) ;
-                    std :: cout << "red , " ;
-                    SetConsoleTextAttribute ( color , 10 ) ;
-                    std :: cout << "green , " ;
-                    SetConsoleTextAttribute ( color , 3 ) ;
-                    std :: cout << "blue or " ;
-                    SetConsoleTextAttribute ( color , 13 ) ;
-                    std :: cout << "purple\n" ;
-                    std :: cout << "Enter the color of player (" << i << ") : " ;
 
-                    SetConsoleTextAttribute ( color , 7 ) ;
-                    std :: cin >> color1 ;
+                    for ( int j = 0 ; j < colorsList.size () ; j ++ ){
 
-                    if ( color1 != "yellow" && color1 != "orange" && color1 != "red" &&
-                         color1 != "green" && color1 != "blue" && color1 != "purple" ){
+                        if ( colorsList [j] == "yellow" ){
+                            SetConsoleTextAttribute ( color , 14 ) ;
+                            std :: cout << colorsList [j] ;
+                        }
+                        else if ( colorsList [j] == "orange" ){
+                            SetConsoleTextAttribute ( color , 6 ) ;
+                            std :: cout << colorsList [j] ;
+                        }
+                        else if ( colorsList [j] == "red" ){
+                            SetConsoleTextAttribute ( color , 12 ) ;
+                            std :: cout << colorsList [j] ;
+                        }
+                        else if ( colorsList [j] == "green" ){
+                            SetConsoleTextAttribute ( color , 10 ) ;
+                            std :: cout << colorsList [j] ;
+                        }
+                        else if ( colorsList [j] == "blue" ){
+                            SetConsoleTextAttribute ( color , 3 ) ;
+                            std :: cout << colorsList [j] ;
+                        }
+                        else if ( colorsList [j] == "purple" ){
+                            SetConsoleTextAttribute ( color , 13 ) ;
+                            std :: cout << colorsList [j] ;
+                        }
+
+                        SetConsoleTextAttribute ( color , 7 ) ;
+                        std :: cout << "  " ;
+                    }
+
+                    std :: cout << "\nEnter the color of player (" << i << ") : " ;
+                    std :: cin >> input ;
+
+                    if ( input != "yellow" && input != "orange" && input != "red" &&
+                         input != "green"  && input != "blue"   && input != "purple" ){
 
                         std :: cout << "Error !\n" ;
                         continue ;
                     }
                     else {
-                        newPlayer.setColor ( color1 ) ;
+                        newPlayer.setColor ( input ) ;
+
+                        for ( int j = 0 ; j < colorsList.size () ; j ++ ){
+                            if ( colorsList [j] == input ){
+                                colorsList [j] = "null" ;
+                            }
+                        }
                         break ;
                     }
                 }
                 players.push_back ( newPlayer ) ;
+                std :: cout << "\n" ;
             }
         }
 
-    private :
-        int numberOfPlayers ;
-        std :: vector < Player & > players ;
+        void game (){
 
+            std :: vector < std :: vector < std :: string > > playersCards ( numberOfPlayers , ( std :: vector < std :: string > (10) ) ) ;
+            bool flag = true ;
+            
+            while ( true ){
+
+                flag = checkCards ( playersCards ) ;
+                if ( flag == true ){}
+            }
+        }
+
+        bool checkCards ( std :: vector < std :: vector < std :: string > > vec ){
+
+            for ( int i = 0 ; i < numberOfPlayers ; i ++ ){
+                for ( int j = 0 ; j < vec [i].size () ; j++ ){
+                    if ( vec [i][j] != "null" ){
+                        return false ;
+                    }
+                }
+            }
+            return true ;
+        }
+
+        void shuffleCards ( std :: vector < std :: vector < std :: string > >& vec ){
+            
+        }
+
+    private :
+
+        int numberOfPlayers = 0 ;
+        int youngestPerson = 0 ;
+        std :: vector < Player > players ;
+        //std :: vector < std :: string > 
 };
